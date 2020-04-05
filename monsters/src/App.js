205b748649062ccca98c27751d008a81849ec684
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-04-04 22:10:16 -0600

    componetDidMount function added

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      mosters : [
-        {name: 'Frankeistein', id: 'abc123'},
-        {name: 'Drakula', id: 'cde234'},
-        {name: 'Zombie', id:'fgh456'}
-      ]
+      monsters : []
     }
-  }
+  } 
+
+componentDidMount () {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters:users }));
+}
 
   render() {
     return (
       <div className="App">
         {
-          this.state.mosters.map(monster => 
+          this.state.monsters.map(monster => 
             <h1 key={monster.id}> { monster.name } </h1>
             )
         }
