f50e94cdb68cff2b37b0b34f4ef042e00e516274
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-06-11 21:35:43 -0600

    search monsters

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {CardList} from './components/card-list/card-list.component';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters : []
+      monsters: [],
+      searchField: ''
     }
-  } 
+  }
 
-componentDidMount () {
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(users => this.setState({ monsters:users }));
-}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+       monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <input type='search'
+          placeholder='search monsters'
+          onChange={e => console.log(this.setState({ searchField : e.target.value }))}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
