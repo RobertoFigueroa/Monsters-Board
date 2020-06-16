4bf2a32a36fdc372eafb4b3ff04ad1482e264158
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-06-16 17:30:04 -0600

    Updates

---
+++ precache-manifest.a8a5543bae2f23c32efb54e76a0ed086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44936a9ab5fe380fc3b6aa4059caab1a",
+    "url": "/monsters-board/index.html"
+  },
+  {
+    "revision": "e039bc421f1017c92e6f",
+    "url": "/monsters-board/static/css/main.5eafb9e1.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-board/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-board/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e039bc421f1017c92e6f",
+    "url": "/monsters-board/static/js/main.44e7744c.chunk.js"
+  },
+  {
+    "revision": "be58d68e7269b7980246",
+    "url": "/monsters-board/static/js/runtime-main.dcf4beff.js"
+  }
+]);
